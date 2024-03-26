081dfb9a5eb755a0ba4e2d1fad9bd63734c0b636
dbalestieri <dbalestieri@mulesoft.com>
2024-03-26 13:39:58 -0300

    Add security token

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import json
 from db import MovieDb
 from flask import Flask
+from security import require_secret
 
 app = Flask(__name__)
 db = MovieDb()
 
 @app.route("/recommendations/movie/<id>")
+@require_secret
 def get_movie_recommendation(id):
   recs = json.loads(db.get_movie_movie_recommendation(id))
   return [{"id": rec} for rec in recs]
