a399e65020b84a62cf65411fac4ee1d20cf8bb39
dbalestieri <dbalestieri@mulesoft.com>
2024-03-30 20:21:02 -0300

    hello world

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-import json
-from db import InvalidIdError, MovieDb
-from flask import Flask, jsonify
-from security import require_secret
-
+from flask import Flask
 app = Flask(__name__)
-db = MovieDb()
 
-@app.route("/recommendations/movie/<id>")
-@require_secret
-def get_movie_recommendation(id):
-  print(id)
-  try:
-    recs = json.loads(db.get_movie_movie_recommendation(id))
-  except InvalidIdError as e:
-    print(str(e))
-    return jsonify({'error': 'Invalid ID: ID must be a positive integer.'}), 400
-  return [{"id": rec} for rec in recs]
-
-""" if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=int("5000"))  """
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
