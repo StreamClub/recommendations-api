dd553ec45dcb49295cfc266df69e77e1c304f16c
Sebastián Capelli <scapelli@fi.uba.ar>
2024-03-31 15:15:13 -0300

    rapi test

--- app.py
+++ app.py
@@ -1,6 +1,57 @@
-from flask import Flask
-app = Flask(__name__)
+from fastapi import FastAPI
+import uvicorn
+import sys
+# from security import require_secret
+from db import InvalidIdError, MovieDb
+from sqlalchemy.orm import Session
 
-@app.route('/')
-def hello_world():
-  return 'Hello, World!'
+from fastapi import HTTPException, Depends
+from starlette import status
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Boolean, text
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+connection_string = os.getenv('DATABASE_URL')
+PORT = os.getenv('PORT')
+
+engine = create_engine(connection_string)
+
+Base = declarative_base()
+
+class MMR(Base):
+    __tablename__ = "movie_movie_recommendation"
+
+    id = Column(Integer,primary_key=True,nullable=False)
+    recommendations = Column(String,nullable=False)
+
+    def __str__(self):
+        return f"Recommendations for movie with id {self.id}: {self.recommendations}"
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = SessionLocal()
+
+app = FastAPI()
+
+@app.get('/')
+async def home():
+  return "Hello from root"
+
+@app.get('/recommendations/movie/{id}')
+# @require_secret
+async def get_movie_recommendation(id:int):
+  
+  result = session.query(MMR).filter(MMR.id == id).first()
+  if result is None:
+    raise HTTPException(status_code=404, detail="Item not found")
+  return result.recommendations
+ 
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=int("8003"))
